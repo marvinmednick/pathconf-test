d360c58ddb3bfaaaaf67332e23ab4f53af647954
Marvin Mednick <mmednick@gmail.com>
2023-02-27 14:23:33 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::ffi::CString;
+
+fn main() {
+    let path = "/".to_string();
+    let path = CString::new(path.into_bytes()).unwrap();
+    for i in 0..=20 {
+        let pc_value = unsafe { libc::pathconf(path.as_ptr(), i) };
+        println!("Hello, world! {} {}", i, pc_value);
+    }
+}
