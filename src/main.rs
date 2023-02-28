9c96a374adc90352089bcc6b8b4c1e2966930738
Marvin Mednick <mmednick@gmail.com>
2023-02-27 18:05:00 -0800

    increasing range for test to -1 to 50

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::ffi::CString;
 fn main() {
     let path = "/".to_string();
     let path = CString::new(path.into_bytes()).unwrap();
-    for i in 0..=20 {
+    for i in -1..=50 {
         let pc_value = unsafe { libc::pathconf(path.as_ptr(), i) };
         println!("Hello, world! {} {}", i, pc_value);
     }
