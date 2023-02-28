fd953eb2a2e1e98386285b70ba05fc3381935b88
Marvin Mednick <mmednick@gmail.com>
2023-02-27 21:53:04 -0800

    Added errno reporting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use nix::errno::{errno, Errno};
 use std::ffi::CString;
 
 fn main() {
     let path = "/".to_string();
     let path = CString::new(path.into_bytes()).unwrap();
-    for i in -1..=50 {
+    for i in -1..=30 {
+        Errno::clear();
         let pc_value = unsafe { libc::pathconf(path.as_ptr(), i) };
-        println!("Hello, world! {} {}", i, pc_value);
+
+        println!("Hello, world! {} {} {}", i, pc_value, errno());
     }
 }
