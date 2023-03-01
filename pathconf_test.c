8e8f3870ce2dcc91884e48e77d41f48e0bece6d6
Marvin Mednick <mmednick@gmail.com>
2023-03-01 11:37:31 -0800

    Added C test

---
+++ pathconf_test.c
@@ -0,0 +1,20 @@
+#define _POSIX_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+void main()
+{
+    int long result;
+
+    for (int i=0; i<30; i++) {
+        result = pathconf("/",i);
+        printf("Index %d is %ld",i,result);
+        if (result == -1) {
+            int errnum = errno;
+            printf("  Error: %d - %s",errno, strerror(errnum));
+        }
+        printf("\n");
+    }
+}
